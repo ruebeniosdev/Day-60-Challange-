e82886135f5ab5baa917c9a1e8ca7de8251b0ddd
Akankobateng Rueben <akankobatengr@gmail.com>
2022-08-16 15:07:07 +0000

    Initial Commit

---
+++ Challenge/ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeApp.swift
+//  Challenge
+//
+//  Created by RUEBEN on 8/16/22.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
